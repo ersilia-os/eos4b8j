32f9f53e89d9a27be055fb5cabcdba554a74093b
Amna Ali <98670206+Amna-28@users.noreply.github.com>
2022-08-15 14:14:30 +0200

    Add files via upload

---
+++ model/framework/code/similarity_search.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+# Input Parameters
+
+input_file = open(sys.argv[1], 'r')
+Lines = input_file.readlines()
+fp     = 'ECfp4'
+db     = 'GDBChEMBL'
+nnc    = '100'
+
+
+data = {}
+for input_smiles in Lines:
+    input_smiles = input_smiles.strip() 
+    url = 'https://gdb-chembl-simsearch.gdb.tools/search?smi=' + input_smiles +  '&fp=' + fp + '&db=' + db + '&nnc=' + nnc
+
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, features = 'html.parser')
+    results = soup.find_all('script')
+    T = str(results[-1]).splitlines()
+    T = [i for i in T if not ('IDX' not in i)]
+    smiles_list = []
+    similarity_indices = []
+    for i in T:
+        x = i.split('+\"')
+        x1 = x[1].split("IDX")
+        x2 = x1[0]
+        smiles_list.append(x2.strip(' ')) 
+        similarity_indices.append(float(x[3].strip('\"')))
+    
+    data[input_smiles] = [smiles_list, similarity_indices]
+
+import json
+with open(sys.argv[2], 'w') as f:
+    json.dump(data, f)
