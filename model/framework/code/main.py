a3a0d5059bc82fd63aab865f5f0bb1ddeed9c066
Marina18 <marina@ersilia.io>
2026-09-01 12:20:56 +0200

    update columns

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -40,7 +40,7 @@ for input_smiles in Lines:
     data += [[sorted_smiles_list, sorted_similarity_indices]]
 
 
-header = [f"smiles_{i:02}" for i in range(len(smiles_list))]
+header = [f"smi_{i:02}" for i in range(len(smiles_list))]
 with open(sys.argv[2], "w") as f:
     writer = csv.writer(f)
     writer.writerow(header)
